86146ba666f8aa330b44efe74d18b97411ab335f
Zachary <zacharyvella@gmail.com>
2023-11-10 08:19:27 +0000

    Created the Company model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     use HasFactory;
-    protected $fillable = ['first_name', 'last_name', 'email', 'address', 'company_id'];
+    protected $fillable = ['first_name', 'last_name', 'email', 'address'];
 }
