f48318d458855dc34e34dbfccdfc605cd1d44f30
Zachary <zacharyvella@gmail.com>
2023-11-13 09:16:08 +0000

    Implement named routes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <div>
     <a href="{{ route('contacts.index') }}">All Contacts</a>
     <a href="{{ route('contacts.create') }}">Add Contact</a>
-    <a href="{{ route('contacts.show') }}">Show contact id 1</a>
+    <a href="{{ route('contacts') }}">Show contact id 1</a>
 </div>
