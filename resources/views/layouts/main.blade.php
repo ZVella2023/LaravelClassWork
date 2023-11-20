c9026e1f1776e6841f43655ec9fc0e46d8f8b4c3
Zachary <zacharyvella@gmail.com>
2023-11-20 07:22:28 +0000

    rendering static data in a table layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -36,9 +36,9 @@
 
     @yield('content')
 
-    <script src="{{ assets('js/jquery.min.js') }}"></script>
-    <script src="{{ assets('js/popper.min.js') }}"></script>
-    <script src="{{ assets(js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
   </body>
 </html>
 
